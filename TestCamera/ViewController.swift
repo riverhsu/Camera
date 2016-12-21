98dfa666376d2c0ef0b2543553c564843b15d542
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-12-21 13:30:08 +0800

    References: https://www.youtube.com/watch?v=jJRNoLfoVHU Purpose: Practice the usage of related camera functions

--- TestCamera/ViewController.swift
+++ TestCamera/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var pickedImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func photoLibAction(_ sender: UIButton) {
+        //Bring up Camera
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
 
+    }
+
+    @IBAction func cameraButtonAction(_ sender: UIButton) {
+        //Bring up Camera
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+
+    }
+    
+    @IBAction func saveButtonAction(_ sender: UIButton) {
+        let imageData = UIImageJPEGRepresentation(pickedImage.image!, 0.6)
+        let compressedJPEGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
+        
+    }
+    
+    //Because simulator nver use Camera, 
+    //tells the delegate that the user picked a still image or movie
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject: AnyObject]! ) {
+        pickedImage.image = image
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+    
 }
 
